b0f4406db3d52d2c7e792476daff487c8a3fa182
Paz Spera <mspera03@gmail.com>
2021-03-03 01:51:10 -0300

    agregué 03-05 a leccion 3

---
+++ Leccion03/03-05OperadoresComparacion.js
@@ -0,0 +1,17 @@
+// ¿Son iguales dos valores? El resultado puede ser verdadero o falso
+let a = 3, b =2, c = "3";
+
+let z = a == c; // se revisa el valor sin importar el tipo
+console.log(z);
+
+z = a === c; // revisa los valores pero tambien los tipos
+console.log(z);
+
+// ¿Son distintos dos valores?
+z = a != c; // se revisa el valor sin importar el tipo
+console.log(z);
+
+z = a !== c; // revisa los valores pero tambien los tipos
+console.log(z);
+
+
