efe6a7590cbc392ff976b4ab0aac21fd02b80e37
Paz Spera <mspera03@gmail.com>
2021-03-03 01:33:53 -0300

    lecciones 01 al 04 de leccion 3

---
+++ Leccion03/03-02-OperadoresJavaScript2.js
@@ -0,0 +1,27 @@
+let a = 3;
+let b =2;
+let z = a + b;
+
+// Incremento
+// Pre-incremento (el operador ++ antes de la variable )
+z = ++a;
+console.log(a);
+console.log(z);
+
+// Post-incremento (el operador ++ después de la variable)
+z = b++;
+console.log(b);
+console.log(z);
+
+// Decremento
+// Predecremento
+z = --a;
+console.log(a);
+console.log(z);
+
+// Postdecremento
+z = b--;
+console.log(b);
+console.log(z);
+
+
---
+++ Leccion03/03-04-OperadoresAsignacion.js
@@ -0,0 +1,20 @@
+let a = 1; 
+
+// Operador de asignación compuesto
+a += 3; // a = a + 3
+console.log(a);
+
+a -=2; // a = a - 2
+console.log(a);
+
+a *= 2; // a = a * 2
+console.log(a);
+
+a /=3;
+console.log(a);
+
+a %= 3;
+console.log(a);
+
+a **= 3;
+console.log(a);
