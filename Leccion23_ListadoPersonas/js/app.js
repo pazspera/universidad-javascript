66d2c798e4f2ee2af0088352b86b4e2c900a62ad
Paz Spera <mspera03@gmail.com>
2021-05-31 23:01:38 -0300

    agregué apuntes Leccion23_ListadoPersonas

---
+++ Leccion23_ListadoPersonas/js/app.js
@@ -0,0 +1,30 @@
+const personas = [
+    new Persona('Clark', 'Kent'),
+    new Persona('Bruce', 'Wayne'),
+];
+
+function mostrarPersonas(){
+    console.log('Mostrar personas...');
+    let texto = '';
+    for(let persona of personas){
+        console.log(persona);
+        texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+    }
+    document.getElementById('personas').innerHTML = texto;
+}
+
+function agregarPersona(){
+    const forma = document.forms['forma'];
+    const nombre = forma['nombre'];
+    const apellido = forma['apellido'];
+    if(nombre.value != '' && apellido.value != ''){
+        const persona = new Persona(nombre.value, apellido.value);
+        console.log(persona);
+        personas.push(persona);
+        mostrarPersonas();
+    } 
+    else {
+        console.log('No hay información que agregar');
+    }
+    
+}
